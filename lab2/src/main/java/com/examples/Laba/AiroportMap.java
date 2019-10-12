e8ab32cb82a8fc8bc7dfde5d856663cfb772536d
Egorychev Artem <temych_2000@mail.ru>
2019-10-12 19:48:19 +0300

    Scripted auto-commit on change (2019-10-12 19:48:19) by gitwatch.sh

--- lab2/src/main/java/com/examples/Laba/AiroportMap.java
+++ lab2/src/main/java/com/examples/Laba/AiroportMap.java
@@ -1,5 +1,7 @@
 package com.examples.Laba;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-public class AiroportMap extends Mapper {
-
+public class AiroportMap extends Mapper <LongWritable, Text,WritableComparable, Text>{
+    
 }
