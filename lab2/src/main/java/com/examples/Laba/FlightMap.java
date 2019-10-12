6f1261a4a44d637ef189723727dff9bbf05d4f66
Egorychev Artem <temych_2000@mail.ru>
2019-10-13 02:03:10 +0300

    Scripted auto-commit on change (2019-10-13 02:03:10) by gitwatch.sh

--- lab2/src/main/java/com/examples/Laba/FlightMap.java
+++ lab2/src/main/java/com/examples/Laba/FlightMap.java
@@ -1,4 +1,15 @@
 package com.examples.Laba;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
 public class FlightMap {
-}
+
+    public class AiroportMap extends Mapper<LongWritable, Text,WritableComparable, Text> {
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        }
