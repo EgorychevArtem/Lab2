9aeeb037007fef90ce4e6ede4501723111830710
Egorychev Artem <temych_2000@mail.ru>
2019-10-12 19:54:17 +0300

    Scripted auto-commit on change (2019-10-12 19:54:17) by gitwatch.sh

--- lab2/src/main/java/com/examples/Laba/AiroportMap.java
+++ lab2/src/main/java/com/examples/Laba/AiroportMap.java
@@ -5,8 +5,13 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class AiroportMap extends Mapper <LongWritable, Text,WritableComparable, Text>{
+public class AiroportMap extends Mapper <LongWritable, Text,WritableComparable, Text> {
     @Override
-    protected void map(LongWritable key,Text value,Context context) throws IOException, InterruptedException {
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String str[] = value.toString().split("\", ");
+        Text Airoport_title = new Text(str[0].replaceAll("\"", ""));
+        int AiroportID = Integer.parseInt(str[0].replaceAll("\"", ""));
 
+        
     }
+}
