41a4de1dc4b4107fde1cc92231ad373b469523de
Egorychev Artem <temych_2000@mail.ru>
2019-10-14 00:50:50 +0300

    Scripted auto-commit on change (2019-10-14 00:50:50) by gitwatch.sh

--- lab2/src/main/java/com/examples/Laba/Reduce.java
+++ lab2/src/main/java/com/examples/Laba/Reduce.java
@@ -6,8 +6,11 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
-public class WordReducer extends Reducer<Text, IntWritable,Text, LongWritable> {
+public class Reduce extends Reducer<Text, IntWritable,Text, LongWritable> {
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-
+        Iterator<>
+    }
+}
