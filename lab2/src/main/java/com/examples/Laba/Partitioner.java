d1b079f60a4c0125f9069479a40d45bbe0cdaafc
Egorychev Artem <temych_2000@mail.ru>
2019-10-14 00:46:56 +0300

    Scripted auto-commit on change (2019-10-14 00:46:56) by gitwatch.sh

--- lab2/src/main/java/com/examples/Laba/Partitioner.java
+++ lab2/src/main/java/com/examples/Laba/Partitioner.java
@@ -3,7 +3,6 @@ package com.examples.Laba;
 import org.apache.hadoop.io.Text;
 
 public class Partitioner {
-
     public int Partitioner(WritableComparable key, Text value, int NumberTask){
         return key.AiroportID % NumberTask;
     }
