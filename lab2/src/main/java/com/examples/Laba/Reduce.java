377420a8414ed677a95c889c269b16da4c052c0c
Egorychev Artem <temych_2000@mail.ru>
2019-10-14 01:09:42 +0300

    Scripted auto-commit on change (2019-10-14 01:09:42) by gitwatch.sh

---
+++ lab2/src/main/java/com/examples/Laba/Reduce.java
@@ -0,0 +1,28 @@
+package com.examples.Laba;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class Reduce extends Reducer<Text, Text,Text, Text> {
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        Iterator<IntWritable> iter = values.iterator();
+        Text AiroportTitle = new Text("Title: " + iter.next().toString());
+        float min = 0.0f, max = 0.0f, sum = 0.0f;
+        while(iter.hasNext()){
+            float current = Float.parseFloat(iter.next().toString());
+            if (current < 0.0f){
+                min = current;
+            } else if(max > current){
+                max = current;
+            }
+            sum += current;
+        }
+        Text Result = new Text("min: " + Float.toString(min) + "max: " + Float.toString(max) + "sum: " + Float.toString(sum));
+        context.write(AiroportTitle, Result);
+    }
+}
