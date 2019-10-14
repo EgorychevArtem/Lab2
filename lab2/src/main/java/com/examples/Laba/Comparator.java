04eec91aef972afbc204405f1c29b7d648888dfd
Egorychev Artem <temych_2000@mail.ru>
2019-10-14 22:33:06 +0300

    Scripted auto-commit on change (2019-10-14 22:33:06) by gitwatch.sh

--- lab2/src/main/java/com/examples/Laba/Comparator.java
+++ lab2/src/main/java/com/examples/Laba/Comparator.java
@@ -4,5 +4,8 @@ public class Comparator extends WritableComparable{
     protected Comparator(){
         super(WritableComparable.class, true);
     }
-    
+
+    public int compare(WritableComparable first, WritableComparable second){
+        return first.compareAiroport(second);
+    }
 }
