08ad12363cc039a182009f3c29e0cb3669a66e66
Egorychev Artem <temych_2000@mail.ru>
2019-10-14 22:21:12 +0300

    Scripted auto-commit on change (2019-10-14 22:21:12) by gitwatch.sh

--- lab2/src/main/java/com/examples/Laba/App.java
+++ lab2/src/main/java/com/examples/Laba/App.java
@@ -1,11 +1,14 @@
 package com.examples.Laba;
 
+import org.apache.hadoop.mapreduce.Job;
+
 public class App {
     public static void main(String[] args){
         if (args.length != 3) {
             System.err.println("Usage: SortApp <input path flight> <input path airport> <output path>");
             System.exit(-1);
-            
+
+            Job job = 
         }
     }
 }
