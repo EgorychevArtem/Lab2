66ebdbceedd312c6a6ba5507b5df5b93975b97b2
Egorychev Artem <temych_2000@mail.ru>
2019-10-14 22:37:27 +0300

    Scripted auto-commit on change (2019-10-14 22:37:27) by gitwatch.sh

--- lab2/src/main/java/com/examples/Laba/Partitioner.java
+++ lab2/src/main/java/com/examples/Laba/Partitioner.java
@@ -2,8 +2,13 @@ package com.examples.Laba;
 
 import org.apache.hadoop.io.Text;
 
-public class Partitioner {
+public class Partitioner extends org.apache.hadoop.mapreduce.Partitioner {
     public int Partitioner(WritableComparable key, Text value, int NumberTask){
         return key.AiroportID % NumberTask;
+    }
+
+    @Override
+    public int getPartition(Object o, Object o2, int i) {
+        return 0;
     }
 }
