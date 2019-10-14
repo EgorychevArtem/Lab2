fd3bce1824132cd2c2b53a02c1029208a927b007
Egorychev Artem <temych_2000@mail.ru>
2019-10-14 22:20:27 +0300

    Scripted auto-commit on change (2019-10-14 22:20:27) by gitwatch.sh

---
+++ lab2/src/main/java/com/examples/Laba/App.java
@@ -0,0 +1,11 @@
+package com.examples.Laba;
+
+public class App {
+    public static void main(String[] args){
+        if (args.length != 3) {
+            System.err.println("Usage: SortApp <input path flight> <input path airport> <output path>");
+            System.exit(-1);
+
+        }
+    }
+}
