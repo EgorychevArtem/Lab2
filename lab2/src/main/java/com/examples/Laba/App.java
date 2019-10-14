4021f093a1c772f374e328f47462f7a27dd2ad17
Egorychev Artem <temych_2000@mail.ru>
2019-10-14 22:21:30 +0300

    Scripted auto-commit on change (2019-10-14 22:21:30) by gitwatch.sh

--- lab2/src/main/java/com/examples/Laba/App.java
+++ lab2/src/main/java/com/examples/Laba/App.java
@@ -8,7 +8,20 @@ public class App {
             System.err.println("Usage: SortApp <input path flight> <input path airport> <output path>");
             System.exit(-1);
 
-            Job job = 
+            Job job = Job.getInstance();
+            job.setJarByClass(JoinJob.class);
+            job.setJobName("JoinJob sort");
+            MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
+            MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+            FileOutputFormat.setOutputPath(job, new Path(args[2]));
+            job.setPartitionerClass(TextPair.FirstPartitioner.class);
+            job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+            job.setReducerClass(JoinReducer.class);
+            job.setMapOutputKeyClass(TextPair.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(Text.class);
+            job.setNumReduceTasks(2);
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
         }
     }
 }
