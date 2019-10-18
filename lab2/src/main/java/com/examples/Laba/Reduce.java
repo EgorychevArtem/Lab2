ecf8cf19dcb2c9db790ee2b22adf45fdf5f92d06
Egorychev Artem <temych_2000@mail.ru>
2019-10-18 19:02:36 +0300

    Scripted auto-commit on change (2019-10-18 19:02:36) by gitwatch.sh

--- lab2/src/main/java/com/examples/Laba/Reduce.java
+++ lab2/src/main/java/com/examples/Laba/Reduce.java
@@ -12,17 +12,19 @@ public class Reduce extends Reducer<WritableComparabl, Text,Text, Text> {
     protected void reduce(WritableComparabl key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text AiroportTitle = new Text("Title: " + iter.next().toString());
-        float min = 0.0f, max = 0.0f, sum = 0.0f;
-        while(iter.hasNext()){
-            float current = Float.parseFloat(iter.next().toString());
-            if (current < 0.0f){
-                min = current;
-            } else if(max > current){
-                max = current;
+        if (iter.hasNext()) {
+            float current, min = 0.0f, max = 0.0f, sum = 0.0f;
+            while (iter.hasNext()) {
+                current = Float.parseFloat(iter.next().toString());
+                if (current < min) {
+                    min = current;
+                } else if (current > max) {
+                    max = current;
+                }
+                sum += current;
             }
-            sum += current;
+            Text Result = new Text("min: " + Float.toString(min) + "max: " + Float.toString(max) + "sum: " + Float.toString(sum));
+            context.write(AiroportTitle, Result);
         }
-        Text Result = new Text("min: " + Float.toString(min) + "max: " + Float.toString(max) + "sum: " + Float.toString(sum));
-        context.write(AiroportTitle, Result);
     }
 }
